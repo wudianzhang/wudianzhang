516ddf2bcc8d59593db81d25df07e2d625c65ea1
吴店长 <65374061+wudianzhang@users.noreply.github.com>
2025-10-05 20:53:32 +0800

    Update perfect_code.py

--- perfect_code.py
+++ perfect_code.py
@@ -1,6 +1,1 @@
-class exit:
-    def __new__(cls):
-        return super().__new__(cls)()
-    def __call__(self):
-        raise Exception('Successful')
-exit()
+class exit: raise Exception('Successful')
